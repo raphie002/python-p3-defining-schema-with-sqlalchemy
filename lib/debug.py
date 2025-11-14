394d87713a0541cc0cc6826758e6a97cbbf2c869
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2025-11-14 13:13:23 +0300

    First commit

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-
-from sqlalchemy import create_engine
+# lib/debug.py
+from sqlalchemy import create_engine # type: ignore
 
 from sqlalchemy_sandbox import Student
 
 engine = create_engine('sqlite:///students.db')
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    import ipdb; ipdb.set_trace() # type: ignore
--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
+# lib/sqlalchemy_sandbox.py
+from sqlalchemy import Column, Integer, String, create_engine # type: ignore
+from sqlalchemy.ext.declarative import declarative_base # type: ignore
 
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
+# Define the Base class for declarative mapping
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    # 1. __tablename__ attribute is required to name the table
+    __tablename__ = 'students'
 
+    # 2. Columns are defined using Column objects as class attributes
+    # 3. 'id' is specified as the primary key
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+# This block ensures that the table is created only when this file is run as a script.
 if __name__ == '__main__':
-    pass
+    # Create the Engine connected to the SQLite file
+    engine = create_engine('sqlite:///students.db')
+    # Create all defined tables in the database (including 'students')
+    Base.metadata.create_all(engine)
